88fa19b21139e88acfb59ea52415e7f210bc0dea
Sumedh Sarnaik <119510841+Sumedh45@users.noreply.github.com>
2022-12-04 20:48:14 +0530

    Add files via upload

---
+++ Alphabets ASCII.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("\nCapital Alphabets- ");
+    for(i=65;i<=90;i++)
+    {
+        printf("\n%c",i);
+    }
+    printf("\nSmall Alphabets- ");
+    for(i=97;i<=122;i++)
+    {
+        printf("\n%c",i);
+    }
+}
